a87d1926480525791f8c5d7a437e9a187f5e9ad5
Tom <tomyitav@gmail.com>
2019-04-27 14:45:33 +0300

    updated codegen

--- src/graphql/resolvers/car.ts
+++ src/graphql/resolvers/car.ts
@@ -1,10 +1,10 @@
-import {CarQueryArgs, Car, UpdateCarNameMutationArgs} from '../../interfaces/types'
+import {QueryCarArgs, Car, MutationUpdateCarNameArgs} from '../../interfaces/types'
 import {CarsService} from '../../services/cars/CarsService'
 import {IAppContext} from '../../interfaces/IAppContext'
 
 const resolveFunctions = {
   Query: {
-    car(_, args: CarQueryArgs, context: IAppContext): Promise<Car[]> {
+    car(_, args: QueryCarArgs, context: IAppContext): Promise<Car[]> {
       const carsService: CarsService = context.carsService
 
       return carsService.getCars(args.name)
@@ -12,7 +12,7 @@ const resolveFunctions = {
   },
 
   Mutation: {
-    updateCarName(_, args: UpdateCarNameMutationArgs, context: IAppContext): Promise<Car> {
+    updateCarName(_, args: MutationUpdateCarNameArgs, context: IAppContext): Promise<Car> {
       const carsService: CarsService = context.carsService
 
       return carsService.updateCarName(args._id, args.newName)
--- src/graphql/resolvers/train.ts
+++ src/graphql/resolvers/train.ts
@@ -1,9 +1,10 @@
-import {TrainQueryArgs, Train} from '../../interfaces/types'
+import {QueryTrainArgs, Train} from '../../interfaces/types'
 import {TrainsService} from '../../services/trains/TrainsService'
 import {IAppContext} from '../../interfaces/IAppContext'
+
 const resolveFunctions = {
   Query: {
-    train(_, args: TrainQueryArgs, context: IAppContext): Train[] {
+    train(_, args: QueryTrainArgs, context: IAppContext): Train[] {
       const trainsModel: TrainsService = context.trainsService
 
       return trainsModel.getTrains(args.name)
